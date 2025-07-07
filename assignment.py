4ebdd24661cbabe5e73b3e18810d261c7ab41483
Sahaj Shrestha <sahajshrestha53@gmail.com>
2025-07-06 23:38:49 -0400

    Working code

--- assignment.py
+++ assignment.py
@@ -1,1 +1,16 @@
-print("Hello, World!")
+def insertion_sort_descending(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] < key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = key
+
+if __name__ == "__main__":
+    data = [9, 13, 7, 21, 4]
+    print("Given Unsorted Array:", data)
+    insertion_sort_descending(data)
+    print("Sorted In Descending Order:", data)
